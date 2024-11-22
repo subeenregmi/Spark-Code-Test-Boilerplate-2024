cc8c4f64b91647117330a5d43e20be3da977d979
Subeen Regmi <subeenregmi123@gmail.com>
2024-11-22 03:23:25 +0000

    Added form functionality

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,54 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import Todo, { TodoType } from './Todo';
 
+function TodoForm({todos, setTodos} : {todos: TodoType[], setTodos: Function}) {
+    // 'todos' state is set as prop to this form element
+
+    async function handleSubmit(event: React.SyntheticEvent) {
+        event.preventDefault(); // Allow CORs and stop page reset on submit
+        
+        const target = event.target as typeof event.target & {
+            title: { value: string };
+            description: { value: string };
+        };
+        const title = target.title.value;
+        const description = target.description.value;
+
+        try {
+            // Create to-do
+            const response = await fetch("http://localhost:8080/", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    "title": title,
+                    "description": description
+                })
+            });
+            const data = await response.json();
+
+            if (response.ok) {
+               console.log(data);
+               let list = todos.slice();
+               list.push(data);
+               setTodos(list); // Updates the to do list after creating a new list
+            }
+
+        } catch (error) {
+            console.error("Error occurred when trying to create a new todo: ", error);
+        }
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+        <input placeholder="Title" name="title" autoFocus={true} />
+        <input placeholder="Description" name="description" />
+        <button>Add Todo</button>
+      </form>
+    );
+}
+
 function App() {
   const [todos, setTodos] = useState<TodoType[]>([]);
 
@@ -31,9 +79,9 @@ function App() {
       </header>
 
       <div className="todo-list">
-        {todos.map((todo) =>
+        {todos.map((todo, i) =>
           <Todo
-            key={todo.title + todo.description}
+            key={i}
             title={todo.title}
             description={todo.description}
           />
@@ -41,12 +89,8 @@ function App() {
       </div>
 
       <h2>Add a Todo</h2>
-      <form>
-        <input placeholder="Title" name="title" autoFocus={true} />
-        <input placeholder="Description" name="description" />
-        <button>Add Todo</button>
-      </form>
-    </div>
+        <TodoForm todos={todos} setTodos={setTodos} />
+   </div>
   );
 }
 
